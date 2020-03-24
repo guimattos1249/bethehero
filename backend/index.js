7fd03fae8207d9317f1c81a7aed41a7e4d195d80
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-03-24 10:40:13 -0300

    using request body

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.get('/users/:id', (req, res) => {
-    const params = req.params;
+app.use(express.json());
 
-    console.log(params);
+app.post('/users', (req, res) => {
+    const body = req.body;
+
+    console.log(body);
 
     return res.json({
         nome: "guilherme",
