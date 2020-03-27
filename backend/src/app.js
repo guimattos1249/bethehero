4a39f52c36834b4877ee428aa1048bd1f054b7cf
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-03-27 19:18:31 -0300

    Last Day, finished :D

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
 
-console.log('Servidor Iniciado');
+app.use(errors());
+
+module.exports = app;
